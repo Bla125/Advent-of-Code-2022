b3e2dd83e5c41677f6bb0cd0ef0111ba7af57af9
Bla125 <51074685+Bla125@users.noreply.github.com>
2022-12-26 14:16:59 +0800

    Add files via upload

---
+++ Python/day3p1.py
@@ -0,0 +1,42 @@
+from zoautil_py import datasets
+
+input = datasets.read('Z00620.ADVINPUT.Y22(DAY3IN)') # read input from dataset
+line = list(input.split('\n')) # split input into list delimited by new line
+line = list(map(str.strip, line)) # strip white space
+
+
+def find_dupicates(lines):
+    left_side = set()
+    right_side = set()
+    sum = 0
+
+    for line in lines:                                  # loop through input line by line
+        for letter in range(len(line)//2):              # loop through first half of line
+            if line[letter] not in left_side:           # get rid of duplicate letters in first half
+                left_side.add(line[letter])             # add unique letters to set
+
+        for letter in range(len(line)//2, len(line)):   # loop through second half of line
+            if line[letter] not in right_side:          # get rid of duplicate letters in second half
+                right_side.add(line[letter])            # add unique letters to set
+
+        for letter in left_side:                        # loop through letters in left side of line
+            if letter in right_side:                    # if any letters in the left side are also in the right side
+                if letter.islower():                    # check if letter is lower case
+                    sum += ord(letter) - 96             # get position of letter in alphabet through ascii arithmetic
+                else:                                   # letter is uppercase
+                    sum += ord(letter) - 38             # get position of letter in alphabet through ascii arithmetic
+
+        # remove all elements of sets
+        left_side.clear()
+        right_side.clear()
+         
+    return sum
+
+answer = find_dupicates(line)
+
+print(answer)
+
+datasets.write('Z00620.ADVPY(OUTPUT)', content = str(answer), append = False)
+
+with open(r'/z/z00620/advpython22/output', 'w') as output_file:
+    output_file.write(str(answer))
---
+++ Python/day3p2.py
@@ -0,0 +1,34 @@
+from zoautil_py import datasets
+
+input = datasets.read('Z00620.ADVINPUT.Y22(DAY3IN)') # read input from dataset
+lines = list(input.split('\n')) # split input intro list delimited by new line
+lines = list(map(str.strip, lines)) # remove white space
+
+# split list into groups of three
+# [thelist[pos:pos + size] for pos in range(0, len(thelist), size)]
+groups = [lines[n:n+3] for n in range(0, len(lines), 3)]
+
+items_dict = {}
+sum = 0
+for group in groups:
+    for elf in group:
+        for item in set(elf): # remove duplicates from each elf
+            if item not in items_dict:
+                items_dict[item] = 1
+            else:
+                items_dict[item] += 1
+
+    in_common = max(items_dict, key=items_dict.get)
+    if in_common.islower():
+        sum += ord(in_common) - 96
+    else:
+        sum += ord(in_common) - 38
+
+    items_dict.clear()
+
+print(sum)
+
+datasets.write('Z00620.ADVPY(OUTPUT)', content = str(sum), append = False)
+
+with open(r'/z/z00620/advpython22/output', 'w') as output:
+    output.write(str(sum))
