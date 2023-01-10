2d868c7e2d53bcd1ae99a32bbc6e60cdecb685c2
Bla125 <51074685+Bla125@users.noreply.github.com>
2023-01-10 22:25:26 +0800

    Add files via upload

---
+++ Python/day4p2.py
@@ -0,0 +1,15 @@
+from zoautil_py import datasets
+
+input = datasets.read('Z00620.ADVINPUT.Y22(DAY4IN)') # read input form dataset
+lines = list(input.split('\n')) # split input into list delimited by new line
+lines = list(map(str.strip, lines)) # remove white space
+line = [i.split(',') for i in lines] 
+nums = [[i.split('-') for i in j] for j in line]
+
+total = 0
+
+for i in range(len(nums)):
+    if (int(nums[i][0][0]) <= int(nums[i][1][1])) and (int(nums[i][1][0]) <= int(nums[i][0][1])):
+        total += 1
+
+print(total)
